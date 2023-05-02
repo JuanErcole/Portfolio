d7c2a518a3f5de24a74f7d0666d1530d31772bf6
Juan Ercole <juan.m.ercole@gmail.com>
2023-05-01 21:42:58 -0300

    navbar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { BrowserRouter } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Experience from "./components/Experience"
+import Feedbacks from "./components/Feedbacks"
+import Tech from "./components/Tech"
+import Works from "./components/Works"
+import Contact from "./components/Contact"
+import { StarsCanvas } from "./components"
+
 
 
 export const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+
+    </BrowserRouter>
   )
 }
